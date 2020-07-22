b55dbf6c3f58788861c1ec92a4b41b59f3e8adf9
Silas Jo <52027648+starmaster1126@users.noreply.github.com>
2020-07-22 04:02:45 +0200

    zother

--- backend/credentials.py
+++
@@ -1,3 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('mongodb+srv://atlasadmin:UK3o667%Zz4m@cluster0-ykwvb.mongodb.net')
--- backend/ut_get_user.py
+++
@@ -1,9 +0,0 @@
-import login_functions as lf 
-
-email = 'asdf@comcast.net'
-res = lf.get_user(email)
-print(res)
-if res:
-	print("true")
-else:
-	print("false")
